191f2dcc12b2188e171981dab2a8249a46d353fe
Maxim Kiyaev <cheezuschrist1337@mail.ru>
2026-02-03 03:39:42 +0300

    draw paddle + integrate himself into field

--- src/pong.c
+++ src/pong.c
@@ -10,6 +10,10 @@
 #define TOP_WALL    0
 #define BOTTOM_WALL 24
 
+typedef struct Paddle Paddle;
+typedef struct Player Player;
+typedef struct Ball Ball;
+
 struct Direction{ 
     int dx; 
     int dy; 
@@ -35,16 +39,19 @@ struct GameState {
     bool type_state; 
 };
 
-char hor = '-';
-char ver = '|'; 
+const char hor = '-';
+const char ver = '|'; 
 
 
-void draw_field() { 
+void draw_field(Paddle paddle1, Paddle paddle2) { 
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
             if (row == TOP_WALL || row == BOTTOM_WALL)
                 printf("%c", hor);
             else if (col == LEFT_WALL || col == RIGHT_WALL)
+                printf("%c", ver);
+            else if ((col == paddle1.x && row >= paddle1.y && row < paddle1.y + 3) ||
+                (col == paddle2.x && row >= paddle2.y && row < paddle2.y + 3))
                 printf("%c", ver);
             else
                 printf(" ");
@@ -53,7 +60,27 @@ void draw_field() {
     }   
 }
 
+void draw_paddle(Paddle paddle){ 
+    for(int i = 0; i < 3; i++) { 
+        printf("%c\n", ver);
+    }
+}
+
+
+Player player1, player2;
+
+void init_game() { 
+    player1.score = 0;
+    player1.paddle.x = 1;
+    player1.paddle.y = 10;
+    
+    player2.score = 0;
+    player2.paddle.x = 78;
+    player2.paddle.y = 10;
+}
+
 int main() { 
-    draw_field();
+    init_game(); 
+    draw_field(player1.paddle, player2.paddle);
     return 0; 
 }
